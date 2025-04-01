e4caaba6134a203f70db08c3ac9c5b4e186d8423
ad1655 <ad1655@student.jamk.fi>
2025-04-01 18:41:09 +0300

    Demo kortteja kaunistettu.

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -11,12 +11,12 @@ const Demo = () => {
                 tässä on muutama:
             </p>
             <div className="grid grid-cols-1 *:p-6 sm:grid-cols-3">
-                <div className="flex max-w-[16ch] flex-col gap-6 rounded-md border border-white/40 bg-linear-to-br/oklch from-white/20 to-white/10 text-center drop-shadow-md backdrop-blur-lg">
+                <div className="flex max-w-[16ch] flex-col gap-6 rounded-md border border-white/40 bg-linear-to-br/oklch from-white/20 to-white/10 text-center drop-shadow-md backdrop-blur-lg transition delay-150 duration-300 ease-out hover:filter-none sm:grayscale sm:hover:scale-110 sm:hover:filter-none">
                     <a href="#top" target="_blank">
                         <img
                             src={fakestore}
                             alt="FakeStore logo."
-                            className="transition delay-150 duration-300 ease-out hover:filter-none sm:grayscale sm:hover:scale-110 sm:hover:filter-none"
+                            className=""
                         />
                     </a>
                     <a href="https://fakestoreapi.com/" target="_blank">
